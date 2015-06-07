b0389ebb629dc7fa6f1be7c5c7c8388655f23bd9
johnnie0924@gmail.com <johnnie0924@gmail.com>
2015-06-07 15:53:10 +0900

    メニュー画面の追加（修正中） https://github.com/johnnie0924/RollingCapsule/issues/4

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public void loadStoryMode()
+	{
+
+	}
+
+	public void loadCollection()
+	{
+		
+	}
+
+	public void loadOption()
+	{
+		
+	}
+
+	public void loadStoryHelp()
+	{
+		
+	}
+
+}
